e6e68cb5277f9b3bf8e60c97df117b66a6834db1
Jordi Ferté <jordi.ferte@gmail.com>
2015-08-20 14:22:07 +0200

    Export Excel + Test affichage formulaire

--- src/GeneratorBundle/Tests/Controller/DefaultControllerTest.php
+++ src/GeneratorBundle/Tests/Controller/DefaultControllerTest.php
@@ -1,13 +1,53 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: Jordi
+ * Date: 10/08/2015
+ * Time: 15:11
+ */
 
 namespace GeneratorBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\BrowserKit\Cookie;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+    /**
+     * @var \Doctrine\ORM\EntityManager
+     */
+    private $em;
+    private $client;
+
+    public function setUp()
     {
-        $this->markTestIncomplete('This test has not been implemented yet.');
+        static::$kernel = static::createKernel();
+        static::$kernel->boot();
+        $this->em = static::$kernel->getContainer()->get('doctrine.orm.entity_manager');
+        $this->client = static::createClient();
+    }
+
+    private function logIn()
+    {
+        $session = $this->client->getContainer()->get('session');
+
+        $firewall = 'secured_area';
+        $token = new UsernamePasswordToken('admin', null, $firewall, array('ROLE_ADMIN'));
+        $session->set('_security_'.$firewall, serialize($token));
+        $session->save();
+
+        $cookie = new Cookie($session->getName(), $session->getId());
+        $this->client->getCookieJar()->set($cookie);
+    }
+
+    public function testGenerationForEvaluator()
+    {
+
+        $this->logIn();
+
+        $crawler = $this->client->request('GET', 'edit/sheet/21680');
+//        dump($client);die();
+        $this->assertTrue(200 === $this->client->getResponse()->getStatusCode());
     }
 }
